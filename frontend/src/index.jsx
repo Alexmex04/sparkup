d47c5471d0c583e3bac42fec2c185bda8091262e
Alejandro Vazquez <alexvaztoro@gmail.com>
2025-10-01 13:40:45 -0600

    UI: ajustes en Home2

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './styles/theme.css';
+import './styles/skin-dark-pro.css' 
+
 
 const root = createRoot(document.getElementById('root'));
 root.render(
